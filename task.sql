606ad05370bbb25583bddd5aef558992b44fe74d
Tania Kolesnik <kolesnik@gmail.com>
2025-12-09 21:30:43 +0000

    update task.sql for CustomerId and ProductID

--- task.sql
+++ task.sql
@@ -2,4 +2,6 @@
 USE ShopDB; 
 CREATE INDEX Name ON Products (Name); 
 CREATE INDEX Email ON Customers (Email); 
+CREATE INDEX CustomerID ON Orders (CustomerID); 
+CREATE INDEX ProductID ON OrderItems (ProductID); 
 
